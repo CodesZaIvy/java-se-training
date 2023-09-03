a19b9192d8fb66694fc36355a59f3a5f27c9d9ec
CodesZaIvy <mosetiivy@gmail.com>
2023-09-03 15:26:28 +0300

    Adjust build depenencies

--- src/main/java/com/systechafrica/App.java
+++ src/main/java/com/systechafrica/App.java
@@ -8,5 +8,6 @@ public class App
     public int add(int a, int b) {
         return a + b;
 
+
     }
 }
--- src/test/java/com/systechafrica/AppTest.java
+++ src/test/java/com/systechafrica/AppTest.java
@@ -2,6 +2,11 @@ package com.systechafrica;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//? default instance 
 
 public class AppTest {
 
@@ -19,9 +24,6 @@ public class AppTest {
         
         Assertions.assertEquals(expected, result, "the sum of 2 + 3 should be 5");
 
-
-  
-}
-
+    }
 
 }
