e0adf88e80062904c0e12e80dc7a0fa2ebdcd9bd
CodesZaIvy <mosetiivy@gmail.com>
2023-09-04 09:12:01 +0300

    dataTypes

---
+++ src/main/java/com/systechafrica/ATMMAchine/atmMachine.java
@@ -0,0 +1,30 @@
+package com.systechafrica;
+
+public class atmMachine{
+    final double INITIAL_BALANCE = 1000.00;
+    final double 
+
+ public static void main(String[] args) {
+    
+ }
+ public void login(){
+
+ }
+ public void presentMenu(){
+
+ }
+ public void balanceCheck(){
+
+ }
+public void executeDeposit(){
+
+}
+public void executeWithdrawal(){
+
+}
+public void executeTransfer(){
+
+}
+public void executeReceipt(){
+
+}
---
+++ src/main/java/com/systechafrica/variables/VariablesDemo.java
@@ -0,0 +1,72 @@
+package com.systechafrica;
+
+import java.util.logging.Logger;
+
+public class VariablesDemo {
+    private static final Logger LOGGER =Logger.getLogger(VariablesDemo.class.getName());
+     //
+   
+    
+    private void variableDeclarations() {
+        //integer variable
+        //? type variableName
+        int score = 0;
+        LOGGER.info("Score interger variable value is" +score);
+    }
+     private void constantsVariable() {
+        //?constant variable
+        //final type variableName =value;
+        final double PI = 3.14159;
+        LOGGER.info("Constant variable "+ PI);
+    }       
+       private void integerTypes(){
+        variableDeclarations();
+        LOGGER.info("Integer Minimum" + Integer.MIN_VALUE);
+        LOGGER.info("Integer Minimum" + Integer.MAX_VALUE);
+
+        LOGGER.info("Long Minimum" + Long.MIN_VALUE);
+        LOGGER.info("Long Maximum" + Long.MAX_VALUE);
+} 
+        private void floatingTypes(){
+    //float type 
+    float floatRate = 2.1457454F;
+    double doubleRate = 2.1457454;
+    LOGGER.info("float value" + floatRate );
+    LOGGER.info("double value" + doubleRate );    
+
+
+ }
+          private void characterTypes(){
+    //character type 
+    char grade = 'A';
+    LOGGER.info("character value" + grade);    
+ }
+       private void booleanTypes(){
+    //boolean type 
+    boolean isPromoted = false;
+    boolean isProcessed = true;
+    LOGGER.info("false value" + isPromoted);  
+     LOGGER.info("true value" + isProcessed);  
+
+
+ }
+ public static void main(String[] args) {
+        VariablesDemo demo = new VariablesDemo();
+
+        demo.variableDeclarations();
+        demo.constantsVariable();
+        demo.integerTypes();
+        demo.floatingTypes();
+        demo.characterTypes();
+        demo.booleanTypes();
+
+ }
+
+}
+
+
+
+
+
+
+    
