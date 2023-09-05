194b297362f825fa76dac6c9cf63b0dab26fe854
CodesZaIvy <mosetiivy@gmail.com>
2023-09-05 06:36:26 +0300

    Comparison Operators

--- src/main/java/com/systechafrica/operators/OperatorsDemo.java
+++ src/main/java/com/systechafrica/operators/OperatorsDemo.java
@@ -6,7 +6,7 @@ import java.util.logging.Logger;
 public class OperatorsDemo {
     private static final Logger LOGGER =Logger.getLogger(OperatorsDemo.class.getName());
 
-    private void arithmeticOperators(){
+    public void arithmeticOperators(){
 
     //Addition Operator
 
@@ -52,7 +52,7 @@ public class OperatorsDemo {
 
 
 }
-    private void assignmentOperators(){
+    public void assignmentOperators(){
         //simple assignment operator  
         int quantity = 4500;
     
@@ -83,11 +83,43 @@ public class OperatorsDemo {
 
         
     }
+    public void comparisonOperators(){
+        int studentOneAge = 20;
+        int studentTwoAge = 25;
 
+        //equality operator
+        boolean isStudentOneAgeEqualToStudentTwoAge = studentOneAge == studentTwoAge;
+     LOGGER.info(" isStudentOneAgeEqualToStudentTwoAge :" + isStudentOneAgeEqualToStudentTwoAge );
+
+            //inequality operator
+        boolean isStudentOneAgeNotEqualToStudentTwoAge = studentOneAge != studentTwoAge;
+     LOGGER.info(" isStudentOneAgeNotEqualToStudentTwoAge :" + isStudentOneAgeNotEqualToStudentTwoAge );
+
+            // less than
+    boolean isStudentOneAgeLessThanStudentTwoAge = studentOneAge < studentTwoAge;
+     LOGGER.info(" isStudentOneAgeLessThanStudentTwoAge :" + isStudentOneAgeLessThanStudentTwoAge );
+
+     // greater than
+       boolean isStudentOneAgeGreaterThanStudentTwoAge = studentOneAge > studentTwoAge;
+     LOGGER.info(" isStudentOneAgeGreaterThanStudentTwoAge :" + isStudentOneAgeGreaterThanStudentTwoAge );
+
+     //less than or equal to 
+       boolean isStudentOneAgeLessThanOrEqualToStudentTwoAge = studentOneAge <= studentTwoAge;
+     LOGGER.info(" isStudentOneAgeLessThanEqualToOrStudentTwoAge :" + isStudentOneAgeLessThanOrEqualToStudentTwoAge);
+     // greater than or equal to
+       boolean isStudentOneAgeGreaterThanOrEqualToStudentTwoAge = studentOneAge >= studentTwoAge;
+     LOGGER.info(" isStudentOneAgeGreaterThanOrEqualToStudentTwoAge :" + isStudentOneAgeGreaterThanOrEqualToStudentTwoAge );
+
+
+
+    }
     public static void main(String[] args) {
         OperatorsDemo app = new OperatorsDemo();
-        app.arithmeticOperators();
-        app.assignmentOperators();
+        //app.arithmeticOperators();
+        //app.assignmentOperators();
+        app.comparisonOperators();
+
+
     }
     
 }
