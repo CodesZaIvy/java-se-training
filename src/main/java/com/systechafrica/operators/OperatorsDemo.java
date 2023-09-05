db45874be70df2c1f4a80edf49c4178ee96c2d85
CodesZaIvy <mosetiivy@gmail.com>
2023-09-05 06:08:14 +0300

    Assignment Operators

--- src/main/java/com/systechafrica/operators/OperatorsDemo.java
+++ src/main/java/com/systechafrica/operators/OperatorsDemo.java
@@ -53,6 +53,34 @@ public class OperatorsDemo {
 
 }
     private void assignmentOperators(){
+        //simple assignment operator  
+        int quantity = 4500;
+    
+        // addition assignment operator
+
+        quantity += 100; // > quantity = quantity + 100;
+        LOGGER.info(" Addition Quantity :" + quantity);
+
+        // subtraction assignment operator
+
+        quantity -= 100; // > quantity = quantity - 100 ;
+        LOGGER.info(" Subtraction Quantity :" + quantity);
+
+        // division assignment operator
+
+        quantity /= 100; // > quantity = quantity / 100 ;
+        LOGGER.info(" Division Quantity :" +  quantity);
+
+        // multiplication assignment operator
+
+        quantity *= 100; // > quantity = quantity * 100 ;
+        LOGGER.info(" Multiplication Quantity :" + quantity);
+
+        // modulo assignment operator
+
+        quantity %= 100; // > quantity = quantity % 100 ;
+        LOGGER.info(" Modulo Quantity :" + quantity);
+
         
     }
 
